7238e74ea6111531d77732b884386e24769266d7
Luna Stadler <luc@spreadshirt.net>
2023-02-28 17:30:49 +0100

    Read rewrite instructions from a config file (!)
    This might make this actually useful!

--- prom-revisionist.go
+++ prom-revisionist.go
@@ -9,21 +9,30 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/promql/parser"
+	"gopkg.in/yaml.v3"
 )
 
 var config struct {
 	Addr        string
 	UpstreamURL string
+	Config      string
 }
 
 func main() {
 	flag.StringVar(&config.Addr, "addr", "localhost:19090", "Address to listen on")
 	flag.StringVar(&config.UpstreamURL, "upstream", "http://localhost:9090", "Upstream Prometheus url")
+	flag.StringVar(&config.Config, "config", "config.yaml", "Config file specifying the rewrites")
 	flag.Parse()
+
+	revisionists, err := parseConfig(config.Config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if flag.NArg() == 1 {
 		expr, err := parser.ParseExpr(os.Args[1])
@@ -33,7 +42,7 @@ func main() {
 
 		fmt.Println("before:", expr.Pretty(0))
 
-		err = parser.Walk(&Revisionist{}, expr, nil)
+		err = parser.Walk(revisionists[0], expr, nil)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -78,19 +87,15 @@ func main() {
 					log.Printf("invalid query %q: %s", query, err)
 				} else {
 					before := expr.String()
-					rev := &Revisionist{}
-					err = parser.Walk(rev, expr, nil)
+					// TODO: support multiple revisionists!
+					err = parser.Walk(revisionists[0], expr, nil)
 					if err != nil {
 						log.Printf("could not rewrite: %s", err)
 					} else {
-						if rev.foundBucket {
-							expr = &parser.BinaryExpr{
-								Op:  parser.MUL,
-								LHS: &parser.NumberLiteral{Val: 1000},
-								RHS: expr,
-							}
+						expr, err = revisionists[0].WrapExpr(expr)
+						if err != nil {
+							log.Printf("could not wrap: %s", err)
 						}
-
 						log.Printf("rewriting!\n%s\n=>\n%s", before, expr.String())
 						req.PostForm.Set("query", expr.String())
 
@@ -212,8 +217,145 @@ func main() {
 	log.Fatal(http.ListenAndServe(config.Addr, nil))
 }
 
+type Config struct {
+	Rewrites []RewriteConfig `yaml:"rewrites"`
+}
+
+type RewriteConfig struct {
+	Name   string         `yaml:"name"`
+	For    *regexp.Regexp `yaml:"-"`
+	ForRaw string         `yaml:"for"`
+	Wrap   struct {
+		Match    *regexp.Regexp `yaml:"-"`
+		With     parser.Expr    `yaml:"-"`
+		MatchRaw string         `yaml:"match"`
+		WithRaw  string         `yaml:"with"`
+	} `yaml:"wrap"`
+	RenameMetrics   map[string]string `yaml:"rename-metrics"`
+	RenameLabels    map[string]string `yaml:"rename-labels"`
+	RewriteMatchers []struct {
+		From    *labels.Matcher `yaml:"-"`
+		To      *labels.Matcher `yaml:"-"`
+		FromRaw string          `yaml:"from"`
+		ToRaw   string          `yaml:"to"`
+	} `yaml:"rewrite-matchers"`
+	DeleteLabels []string `yaml:"delete-labels"`
+}
+
+func parseConfig(path string) ([]*Revisionist, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("open config: %w", err)
+	}
+	defer f.Close()
+
+	var config Config
+	dec := yaml.NewDecoder(f)
+	dec.KnownFields(true)
+	err = dec.Decode(&config)
+	if err != nil {
+		return nil, fmt.Errorf("parsing config: %w", err)
+	}
+
+	revisionists := make([]*Revisionist, 0, len(config.Rewrites))
+	for i, rewrite := range config.Rewrites {
+		config.Rewrites[i].For, err = regexp.Compile(rewrite.ForRaw)
+		if err != nil {
+			return nil, fmt.Errorf("invalid rewrites[%d].for regexp: %w", i, err)
+		}
+
+		config.Rewrites[i].Wrap.Match, err = regexp.Compile(rewrite.Wrap.MatchRaw)
+		if err != nil {
+			return nil, fmt.Errorf("invalid rewrites[%d].wrap.match regexp: %w", i, err)
+		}
+
+		config.Rewrites[i].Wrap.With, err = parser.ParseExpr(rewrite.Wrap.WithRaw)
+		if err != nil {
+			return nil, fmt.Errorf("invalid rewrites[%d].wrap.with promtheus expr: %w", i, err)
+		}
+
+		for j, matcher := range rewrite.RewriteMatchers {
+			matchers, err := parser.ParseMetricSelector(matcher.FromRaw)
+			if err != nil {
+				return nil, fmt.Errorf("invalid rewrites[%d].rewrite-matchers[%d].from label matcher %q: %w", i, j, matcher.FromRaw, err)
+			}
+
+			if len(matchers) != 1 {
+				return nil, fmt.Errorf("invalid rewrites[%d].rewrite-matchers[%d].from label matcher %q: must contain only one label matcher", i, j, matcher.FromRaw)
+			}
+
+			config.Rewrites[i].RewriteMatchers[j].From = matchers[0]
+
+			matchers, err = parser.ParseMetricSelector(matcher.ToRaw)
+			if err != nil {
+				return nil, fmt.Errorf("invalid rewrites[%d].rewrite-matchers[%d].to label matcher %q: %w", i, j, matcher.ToRaw, err)
+			}
+
+			if len(matchers) != 1 {
+				return nil, fmt.Errorf("invalid rewrites[%d].rewrite-matchers[%d].to label matcher %q: must contain only one label matcher", i, j, matcher.ToRaw)
+			}
+
+			config.Rewrites[i].RewriteMatchers[j].To = matchers[0]
+		}
+
+		revisionists = append(revisionists, &Revisionist{config: config.Rewrites[i]})
+	}
+
+	return revisionists, nil
+}
+
 type Revisionist struct {
-	foundBucket bool
+	config RewriteConfig
+}
+
+func (r *Revisionist) ShouldProcess(expr parser.Expr) bool {
+	return r.config.For.MatchString(expr.Pretty(0))
+}
+
+func (r *Revisionist) WrapExpr(expr parser.Expr) (parser.Expr, error) {
+	log.Printf("WRAP? %v %q", r.config.Wrap.Match, r.config.Wrap.MatchRaw)
+	log.Println(r.config.Wrap.Match.MatchString(expr.Pretty(0)), expr.Pretty(0))
+
+	if r.config.Wrap.Match == nil || !r.config.Wrap.Match.MatchString(expr.Pretty(0)) {
+		return expr, nil
+	}
+
+	log.Println("wrapping!")
+
+	wrapped, err := parser.ParseExpr(r.config.Wrap.WithRaw)
+	if err != nil {
+		return nil, fmt.Errorf("invalid wrap prometheus expr: %w", err)
+	}
+
+	err = parser.Walk(&wrapper{inner: expr}, wrapped, nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not wrap prometheus expr: %w", err)
+	}
+
+	return wrapped, nil
+}
+
+type wrapper struct {
+	inner parser.Expr
+}
+
+func (w *wrapper) Visit(node parser.Node, path []parser.Node) (parser.Visitor, error) {
+	if node == nil && path == nil {
+		return nil, nil
+	}
+
+	switch val := node.(type) {
+	case *parser.BinaryExpr:
+		if val.LHS.String() == "unwrapped_query" {
+			val.LHS = w.inner
+		} else {
+			val.RHS = w.inner
+		}
+
+		return nil, nil
+	default:
+		return nil, fmt.Errorf("unhandled node %#T", node)
+	}
 }
 
 func (r *Revisionist) Visit(node parser.Node, path []parser.Node) (parser.Visitor, error) {
@@ -224,49 +366,48 @@ func (r *Revisionist) Visit(node parser.Node, path []parser.Node) (parser.Visito
 	switch val := node.(type) {
 	case *parser.AggregateExpr:
 		for i, label := range val.Grouping {
-			if label == "service_name" {
-				val.Grouping[i] = "service"
-			}
-
-			if label == "operation" {
-				val.Grouping[i] = "uri"
+			replacement, ok := r.config.RenameLabels[label]
+			if ok {
+				val.Grouping[i] = replacement
 			}
 		}
 	case *parser.VectorSelector:
-		if val.Name == "calls_total" {
-			val.Name = "http_server_requests_seconds_count"
-		}
-		if val.Name == "latency_bucket" {
-			val.Name = "http_server_requests_seconds_bucket"
-
-			r.foundBucket = true
+		replacement, ok := r.config.RenameMetrics[val.Name]
+		if ok {
+			val.Name = replacement
 		}
 
 		matchers := make([]*labels.Matcher, 0, len(val.LabelMatchers))
 		for _, label := range val.LabelMatchers {
-			if label.Name == "__name__" && label.Value == "calls_total" {
-				label.Value = "http_server_requests_seconds_count"
-			}
-			if label.Name == "__name__" && label.Value == "latency_bucket" {
-				label.Value = "http_server_requests_seconds_bucket"
-
-				r.foundBucket = true
+			if label.Name == "__name__" {
+				replacement, ok := r.config.RenameMetrics[label.Value]
+				if ok {
+					label.Value = replacement
+				}
 			}
 
-			if label.Name == "service_name" {
-				label.Name = "service"
+			replacement, ok := r.config.RenameLabels[label.Name]
+			if ok {
+				label.Name = replacement
 			}
 
-			if label.Name == "status_code" && label.Value == "STATUS_CODE_ERROR" {
-				// label.Type = labels.MatchNotEqual
-				// label.Name = "outcome"
-				// label.Value = "SUCCESS"
-				label.Type = labels.MatchEqual
-				label.Value = "outcome"
-				label.Value = "SERVER_ERROR"
+			for _, rewrite := range r.config.RewriteMatchers {
+				if rewrite.To.Type == label.Type && rewrite.From.Name == label.Name && rewrite.From.Value == label.Value {
+					label.Type = rewrite.To.Type
+					label.Name = rewrite.To.Name
+					label.Value = rewrite.To.Value
+				}
 			}
 
-			if label.Name == "span_kind" {
+			shouldDelete := false
+			for _, deleteLabel := range r.config.DeleteLabels {
+				if label.Name == deleteLabel {
+					shouldDelete = true
+					break
+				}
+			}
+
+			if shouldDelete {
 				continue
 			}
 
